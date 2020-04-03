ed762cbeec829e8f3ca451c76adfe894e01e6c9f
Dima Kuznetsov <dmitrykuzn@gmail.com>
2020-04-03 11:32:32 +0300

    Add analysis of relations between syscalls

--- harness/analysis.py
+++ harness/analysis.py
@@ -1,4 +1,5 @@
 import dataclasses
+import re
 import statistics
 import typing
 
@@ -7,7 +8,7 @@ from harness import syscall_info
 _SYSCALLS = {}
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True, order=True)
 class Syscall:
     nr: int
     name: str
@@ -27,12 +28,36 @@ class SyscallInvocation:
     syscall: Syscall
     start_ns: int
     duration_ns: int
+    ustack_id: int
     userspace_before: int = -1
     userspace_after: int = -1
+    ustack: typing.Tuple[str] = None
 
     @property
     def end_ns(self):
         return self.start_ns + self.duration_ns
+
+    @property
+    def syscall_loc(self):
+        return (self.syscall, self.ustack_id)
+
+
+@dataclasses.dataclass
+class SpanInfo:
+    pairs: typing.List[
+        typing.Tuple[
+            SyscallInvocation,
+            SyscallInvocation,
+        ],
+    ] = dataclasses.field(
+        default_factory=list,
+    )
+
+    def quantiles(self, n=10):
+        return statistics.quantiles(
+            (p[0].userspace_after for p in self.pairs),
+            n=n,
+        )
 
 
 @dataclasses.dataclass
@@ -40,6 +65,16 @@ class ThreadTrace:
     invocations: typing.List[SyscallInvocation] = dataclasses.field(
         default_factory=list,
     )
+    ustacks: typing.Dict[int, typing.Tuple[str]] = dataclasses.field(
+        default_factory=dict,
+    )
+    userspace_spans: typing.Dict[
+        typing.Tuple[
+            typing.Tuple[Syscall, int],
+            typing.Tuple[Syscall, int],
+        ],
+        SpanInfo,
+    ] = dataclasses.field(default_factory=dict)
 
     def calculate_userspace_times(self):
         for a, b in zip(self.invocations, self.invocations[1:]):
@@ -70,9 +105,21 @@ class Trace:
     )
 
 
-def build_trace(rows):
+def build_trace(f):
     trace = Trace()
-    for row in rows:
+    headers = next(f).strip().split(',')
+
+    def _to_row(line):
+        line = line.strip()
+        return dict(zip(headers, (int(e, 16) for e in line.split(','))))
+
+    for row in f:
+        if not row.strip():
+            continue
+        if row.startswith('===='):
+            break
+
+        row = _to_row(row)
         try:
             sc = Syscall.from_nr(row['sysnr'])
         except KeyError:
@@ -81,9 +128,32 @@ def build_trace(rows):
             syscall=sc,
             start_ns=row['tbegin'],
             duration_ns=row['dur'],
+            ustack_id=row['ustack']
         )
         tid = row['tid']
         trace.threads.setdefault(tid, ThreadTrace()).invocations.append(sci)
+
+    for tid, stack, stack_id in re.findall(
+        r'@ustacks\[(\d+),([^\]]*)\]: (\d+)',
+        f.read(),
+        re.M
+    ):
+        tid = int(tid)
+        stack = tuple(l.strip() for l in stack.split())
+        stack_id = int(stack_id)
+        trace.threads[tid].ustacks[stack_id] = stack
+
+    for tid, thread in trace.threads.items():
+        for inv in thread.invocations:
+            inv.ustack = thread.ustacks[inv.ustack_id]
+
+        for inv1, inv2 in zip(thread.invocations, thread.invocations[1:]):
+            thread.userspace_spans.setdefault(
+                (inv1.syscall_loc, inv2.syscall_loc),
+                SpanInfo(),
+            ).pairs.append((inv1, inv2))
+
     for thread in trace.threads.values():
         thread.calculate_userspace_times()
+
     return trace
--- harness/main.py
+++ harness/main.py
@@ -1,8 +1,12 @@
+import argparse
 import dataclasses
+import itertools
+import json
 import logging
 import os
 import pathlib
 import shlex
+import statistics
 import subprocess
 import tempfile
 import time
@@ -10,13 +14,13 @@ import typing
 import sys
 
 import dacite
+import graphviz
 import pandas
 from plotly import graph_objects
 import yaml
 
 from harness import analysis
 from harness import podman
-from harness import syscall_info
 
 LOG = logging.getLogger(__name__)
 
@@ -30,9 +34,16 @@ class Container:
 
 
 @dataclasses.dataclass
+class TracerInfo:
+    bpftrace_script: str
+    output_filename: str
+    output_format: str
+
+
+@dataclasses.dataclass
 class RunInfo:
     container: Container
-    bpftrace_script: str
+    tracers: typing.List[TracerInfo]
 
 
 def discover_workload_uid(cont):
@@ -42,67 +53,83 @@ def discover_workload_uid(cont):
     return int(next(rows))
 
 
-def get_bfptrace_output(run_info, path):
+def get_bfptrace_output(run_info, output_dir):
     with podman.running_container(
-        'postgres',
-        name='test-postgres',
-        env={'POSTGRES_PASSWORD': 'password'},
+        run_info.container.image,
+        env=run_info.container.env,
     ) as cont:
         # FIXME check that container is ready
         time.sleep(3)
         for command in run_info.container.setup_commands:
             podman.execute(cont, shlex.split(command))
         uid = discover_workload_uid(cont)
-        with subprocess.Popen(
-            (
-                'sudo',
-                'bpftrace',
-                '-o', path, run_info.bpftrace_script,
-                str(uid)
-            ),
-            preexec_fn=os.setpgrp,
-            stderr=subprocess.DEVNULL,
-        ) as proc:
-            for command in run_info.container.test_commands:
-                LOG.debug('Running test command: %s', command)
-                podman.execute(cont, shlex.split(command))
 
-            # sudo processes require special measures, as sudo won't relay
-            # signal if command was not started on a pty
-            # FIXME later to avoid killing other tracers
-            subprocess.run(('sudo', 'pkill', 'bpftrace'), check=True)
-            proc.wait()
+        # Start all tracers
+        tracers = []
+        for ti in run_info.tracers:
+            tracers.append(
+                subprocess.Popen(
+                    (
+                        'sudo',
+                        'bpftrace',
+                        '-f', ti.output_format,
+                        '-o', output_dir / ti.output_filename,
+                        ti.bpftrace_script,
+                        str(uid)
+                    ),
+                    preexec_fn=os.setpgrp,
+                    # stderr=subprocess.DEVNULL,
+                ),
+            )
+        for command in run_info.container.test_commands:
+            LOG.debug('Running test command: %s', command)
+            podman.execute(cont, shlex.split(command))
+
+        # sudo processes require special measures, as sudo won't relay
+        # signal if command was not started on a pty
+        # FIXME later to avoid killing other tracers
+        subprocess.run(('sudo', 'pkill', 'bpftrace'), check=True)
+        for p in tracers:
+            p.wait()
 
 
 def build_trace(path):
     with open(path) as f:
         # skip first line:
         next(f)
-
-        headers = next(f).strip().split(',')
-
-        def _to_row(line):
-            line = line.strip()
-            return dict(zip(headers, (int(e, 16) for e in line.split(','))))
-
-        return analysis.build_trace(_to_row(l) for l in f if l.strip())
+        trace = analysis.build_trace(f)
+    return trace
 
 
 _USER_Y = 1000
 
 
 def generate_syscall_plot(invocations):
+    syscall_map = {}
+    for invocation in invocations:
+        if invocation.syscall.nr not in syscall_map:
+            syscall_map[invocation.syscall.nr] = invocation.syscall.name
+    syscall_remap = dict(
+        zip(
+            syscall_map.values(),
+            itertools.count(
+                10,
+                900 // len(syscall_map),
+            ),
+        ),
+    )
+
     x, y = [0], [_USER_Y]
     for invocation in invocations:
         # Downward edge:
         x.append(invocation.start_ns - 1)
         y.append(_USER_Y)
         x.append(invocation.start_ns)
-        y.append(invocation.syscall.nr)
+        y.append(syscall_remap[invocation.syscall.name])
 
         # Upward edge:
         x.append(invocation.end_ns)
-        y.append(invocation.syscall.nr)
+        y.append(syscall_remap[invocation.syscall.name])
         x.append(invocation.end_ns + 1)
         y.append(_USER_Y)
 
@@ -115,19 +142,21 @@ def generate_syscall_plot(invocations):
     fig = graph_objects.Figure(
         graph_objects.Scatter(x=df['time'], y=df['exec']),
     )
+
     fig.update_layout(
         yaxis={
             'tickmode': 'array',
-            'tickvals': [_USER_Y] + list(syscall_info.syscall_map.keys()),
+            'tickvals': [_USER_Y] + list(syscall_remap.values()),
             'ticktext': [
                 'userspace',
-            ] + list(syscall_info.syscall_map.values()),
-        }
+            ] + list(syscall_remap.keys()),
+        },
+        xaxis_tickformat='',
     )
-    fig.show()
+    return fig
 
 
-def run(run_info):
+def run(run_info, plot_path):
     with tempfile.TemporaryDirectory(suffix='bfptrace-res') as tmpdir:
         output = pathlib.Path(tmpdir) / 'raw-output'
         get_bfptrace_output(run_info, output)
@@ -140,16 +169,257 @@ def run(run_info):
             f"{tid}, {len(thread.invocations)}, {thread.average_interval}",
         )
 
-        generate_syscall_plot(thread.invocations)
+        fig = generate_syscall_plot(thread.invocations)
+        fig.write_html(plot_path)
         # print(thread.userspace_time_quantiles)
 
 
-def main():
-    with open(sys.argv[1]) as f:
+def produce_bpftrace_output(opts):
+    logging.basicConfig(level=logging.DEBUG)
+    with open(opts.spec) as f:
         run_info = dacite.from_dict(RunInfo, yaml.safe_load(f))
-    run(run_info)
+    os.makedirs(opts.output, exist_ok=True)
+    get_bfptrace_output(run_info, opts.output)
+
+
+def parse_stack(strval):
+    return strval.strip().split()
+
+
+def stacktree_add(st, ustack, syscall, count):
+    for frame in ustack[::-1]:
+        st = st.setdefault(frame, {})
+    st[syscall] = count
+
+
+def parse_ustacks(opts):
+    st = {}
+    with open(opts.input_path) as f:
+        for l in f:
+            o = json.loads(l)
+            if o['type'] != 'map':
+                continue
+            for k, v in o['data'].items():
+                syscall = k
+                for k, v in v.items():
+                    if v < 100:
+                        continue
+                    k = k.split(',')
+                    ustack = parse_stack(k[1])
+                    stacktree_add(st, ustack, syscall, v)
+    dot = graphviz.Digraph()
+    dot.attr(label='Stack graph')
+    dot.attr(labelloc='top')
+    dot.node()
+    print(len(st))
+
+
+@dataclasses.dataclass
+class Node:
+    value: typing.List[str]
+    in_edges: typing.Set[str]
+    out_edges: typing.Set[str]
+    removed: int = 0
+
+    @property
+    def key(self):
+        return min(self.value)
+
+    @property
+    def pretty_name(self):
+        if len(self.value) == 1:
+            return self.value[0]
+        elif len(self.value) == 2:
+            return f'{self.value[0]}\n{self.value[1]}'
+        else:
+            return (
+                f'{self.value[0]}\n' +
+                f'...({len(self.value) - 2})...\n' +
+                f'{self.value[-1]}'
+            )
+
+
+def stack_graph(thread):
+    nodes = {}
+    for stack in thread.ustacks.values():
+        print('->'.join(s[-5:] for s in stack))
+        for frame in stack:
+            nodes[frame] = Node([frame], set(), set())
+
+    for stack in thread.ustacks.values():
+        for ftop, fbot in zip(stack, stack[1:]):
+            nodes[ftop].in_edges.add(fbot)
+            nodes[fbot].out_edges.add(ftop)
+    while True:
+        if not compress_one_edge(nodes):
+            break
+    return nodes
+
+
+def compress_one_edge(nodes):
+    for head in nodes.values():
+        # Try folding head into tail
+        if len(head.in_edges) != 1:
+            continue
+        tail_id = next(iter(head.in_edges))
+        tail = nodes[tail_id]
+
+        if len(tail.out_edges) > 1:
+            continue
+        if tail.out_edges != {head.key}:
+            raise RuntimeError()
+
+        new_node = Node(
+            tail.value + head.value,
+            tail.in_edges,
+            head.out_edges,
+        )
+
+        for v in head.out_edges:
+            v = nodes[v]
+            v.in_edges.discard(head.key)
+            v.in_edges.add(new_node.key)
+
+        for v in tail.in_edges:
+            v = nodes[v]
+            v.out_edges.discard(tail.key)
+            v.out_edges.add(new_node.key)
+
+        del nodes[head.key]
+        del nodes[tail.key]
+        head.removed = 1
+        tail.removed = 1
+        nodes[new_node.key] = new_node
+        return True
+    return False
+
+
+def _stack_diff(s1, s2):
+    s1 = s1[::-1]
+    s2 = s2[::-1]
+    comb = zip(s1, s2)
+    fcomb = list(filter(lambda a: a[0] == a[1], comb))
+    return len(s1) - len(fcomb),  len(s2) - len(fcomb)
+
+
+def _inv_node(inv):
+    return f'{inv.syscall.name}-{inv.ustack_id}'
+
+
+def build_thread_graph(thread, draw_userspace=True):
+    dot = graphviz.Digraph(strict=True)
+    nodes = stack_graph(thread)
+    drawn_userspace = set()
+
+    for ((s1, u1), (s2, u2)), info in thread.userspace_spans.items():
+        i1, i2 = info.pairs[0]
+        if len(info.pairs) < 1000:
+            continue
+        quantiles = info.quantiles()
+        if quantiles[0] > 100000:
+            continue
+
+        for inv in (i1, i2):
+            median_duration = statistics.median(
+                i.duration_ns
+                for i in thread.invocations
+                if i.syscall == inv.syscall
+            )
+            label = (
+                _inv_node(inv) + '\n' +
+                f'{median_duration}'
+            )
+            dot.node(
+                _inv_node(inv),
+                fontcolor='red',
+                label=label,
+            )
+            if draw_userspace:
+                for frame in inv.ustack[::-1]:
+                    us_node = next(
+                        n for n in nodes.values() if frame in n.value
+                    )
+                    dot.node(
+                        us_node.pretty_name,
+                    )
+                    drawn_userspace.add(us_node.key)
+
+                dot.edge(
+                    us_node.pretty_name,
+                    _inv_node(inv),
+                )
+
+        label = (
+            f'{_stack_diff(i1.ustack, i2.ustack)}\n' +
+            f'[{quantiles[0]}, {quantiles[3]}]'
+        )
+        dot.edge(
+            _inv_node(i1),
+            _inv_node(i2),
+            style='dashed',
+            label=label,
+        )
+
+    # Fill in userspace edges
+    if draw_userspace:
+        for key in drawn_userspace:
+            tail = nodes[key]
+            for headkey in drawn_userspace.intersection(tail.out_edges):
+                head = nodes[headkey]
+                dot.edge(tail.pretty_name, head.pretty_name)
+
+    return dot
+
+
+def dump_dot(dot, path):
+    subprocess.run(
+        ('dot', '-Tpng', '-o', path),
+        input=dot.source,
+        encoding='utf8',
+    )
+    print(f'file://{path}')
+
+
+def analyze_syscalls(opts):
+    trace = build_trace(opts.input_path)
+    tid, thread = max(
+        trace.threads.items(),
+        key=lambda kv: len(kv[1].invocations),
+    )
+    dot = build_thread_graph(thread, draw_userspace=True)
+    dump_dot(dot, '/tmp/res.png')
+    for k, v in sorted(thread.userspace_spans.items()):
+        if len(v.pairs) < 1000:
+            continue
+        quantiles = v.quantiles()
+        if quantiles[0] > 100000:
+            continue
+        print(k, quantiles)
+
+
+def main():
+    parser = argparse.ArgumentParser(sys.argv[0])
+    subparsers = parser.add_subparsers()
+    parser_trace = subparsers.add_parser('trace-container')
+    parser_trace.add_argument('spec', help='YAML containing env description')
+    parser_trace.add_argument(
+        'output',
+        help='output directory',
+        type=pathlib.Path,
+    )
+    parser_trace.set_defaults(func=produce_bpftrace_output)
+
+    parser_ustacks = subparsers.add_parser('parse-ustacks')
+    parser_ustacks.add_argument('input_path', type=pathlib.Path)
+    parser_ustacks.set_defaults(func=parse_ustacks)
+
+    parser_syscalls = subparsers.add_parser('analyze-syscalls')
+    parser_syscalls.add_argument('input_path', type=pathlib.Path)
+    parser_syscalls.set_defaults(func=analyze_syscalls)
+
+    opts = parser.parse_args()
+    opts.func(opts)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
     main()
